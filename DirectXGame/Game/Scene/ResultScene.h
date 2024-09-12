19f0e0c25d4c019fd5a57b113d757eb1d4f295a2
aoaomidri <aoaomidori2723@gmail.com>
2024-09-12 10:56:04 +0900

    リザルトの概念追加

---
+++ DirectXGame/Game/Scene/ResultScene.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "IScene.h"
+#include <memory>
+#include <list>
+
+#include "Sprite.h"
+#include "Object3d.h"
+#include "Particle.h"
+#include"Character/Player/Player.h"
+#include "Camera.h"
+#include "WorldTransform.h"
+#include"Camera/DebugCamera.h"
+#include"Camera/FollowCamera.h"
+#include"CollisionManager.h"
+#include"Character/Sandbag/Sandbag.h"
+
+class ResultScene : public IScene {
+public:
+	void Init()override;
+
+	void Update()override;
+
+	void DrawBackGround()override;
+
+	void DrawModel()override;
+
+	void DrawParticleModel()override;
+
+	void DrawParticle()override;
+
+	void DrawUI()override;
+
+	void DrawPostEffect()override;
+
+	void DrawRenderTexture()override;
+
+	void DebugGUI()override;
+
+	~ResultScene()override;
+
+private:
+	Camera camera_;
+
+	PointLight pointLight_;
+	SpotLight spotLight_;
+private:
+
+	std::unique_ptr<DebugCamera> debugCamera_;
+	std::unique_ptr<FollowCamera> followCamera_;
+
+
+private:
+	//モデルマネージャー
+	ModelManager* modelManager_;
+
+	//鏡餅
+	std::unique_ptr<Sandbag> sandbag_;
+	//鏡餅置くよう用
+	std::unique_ptr<Object3d> shelfobj_;
+
+};
+
